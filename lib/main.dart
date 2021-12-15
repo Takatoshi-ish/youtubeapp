804a1fefd099c1760bb2debe07554803d11e4f9c
Takatoshi-ish <ishiguro.taka@gmail.com>
2021-12-15 09:25:24 +0900

    画面遷移

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtubeapp/video_detail_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -87,6 +88,13 @@ class MyApp extends StatelessWidget {
                   itemCount: items.length,
                   itemBuilder: (BuildContext context, int index) {
                     return ListTile(
+                      onTap: () async{
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => VideoDetailPage()),
+                        );
+                      },
                       contentPadding: EdgeInsets.all(8.0),
                       leading: Image.network(
                         'https://storage.googleapis.com/stateless-kboy-salon-com/2020/05/6f797226-flutter%E3%82%B5%E3%83%A0%E3%83%8D.png'
