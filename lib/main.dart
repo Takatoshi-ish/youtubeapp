56b573c7c5145c76c943728aa3e8c72fe7f4e20c
Takatoshi-ish <ishiguro.taka@gmail.com>
2021-12-15 09:13:55 +0900

    サムネイル一覧作成

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
+  final items = List<String>.generate(10000, (i) => "item $i");
 
   @override
   Widget build(BuildContext context) {
@@ -43,41 +43,70 @@ class MyApp extends StatelessWidget {
         body: Container(
           child: Column(
             children: <Widget>[
-              Row(
-                children: <Widget>[
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child:Image.network(
-                      'https://avatars.githubusercontent.com/u/65013756?s=200&v=4'
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      const Text(
-                        'YoutubeApp'
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 60,
+                      height: 60,
+                      child:Image.network(
+                        'https://avatars.githubusercontent.com/u/65013756?s=200&v=4'
                       ),
-                      FlatButton(
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.video_call,
-                              color: Colors.red,
-                            ),
-                            Text('登録する'),
-                          ],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        const Text(
+                          'YoutubeApp'
                         ),
-                        onPressed: (){
-                          // todo
-                        },
-                      )
-                    ],
-                  ),
-                ],
-              )
+                        FlatButton(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.video_call,
+                                color: Colors.red,
+                              ),
+                              Text('登録する'),
+                            ],
+                          ),
+                          onPressed: (){
+                            // todo
+                          },
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: items.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      contentPadding: EdgeInsets.all(8.0),
+                      leading: Image.network(
+                        'https://storage.googleapis.com/stateless-kboy-salon-com/2020/05/6f797226-flutter%E3%82%B5%E3%83%A0%E3%83%8D.png'
+                      ),
+                      title: Column(
+                        children: <Widget>[
+                          Text('[Flutter入門] youtubeアプリ画面を作る方法'),
+                          Row(
+                            children: <Widget>[
+                              Text('267回再生'),
+                              Text('5日前'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      trailing:  Icon(Icons.more_vert),
+                    );
+                  }
+                ),
+              ),
             ],
           ),
         ),
